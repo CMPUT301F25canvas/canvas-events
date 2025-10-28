3d1967f1e3a741a18f45c1479d11712ff9c6c507
FarazOghbaei-ML <oghbaei@ualberta.ca>
2025-10-27 22:40:00 -0600

    db: add Firestore rules and clean backups

--- code/app/build.gradle.kts
+++ code/app/build.gradle.kts
@@ -5,9 +5,7 @@ plugins {
 
 android {
     namespace = "com.example.lotteryeventsystem"
-    compileSdk {
-        version = release(36)
-    }
+    compileSdk = 36
 
     defaultConfig {
         applicationId = "com.example.lotteryeventsystem"
@@ -29,8 +27,8 @@ android {
         }
     }
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_11
-        targetCompatibility = JavaVersion.VERSION_11
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
     }
 }
 

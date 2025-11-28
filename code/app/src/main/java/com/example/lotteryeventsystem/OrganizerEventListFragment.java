d9f4b071d397eef214fcbd9215449ebde3e0c8fa
Ethan Kinch <epkinch@gmail.com>
2025-11-28 15:29:25 -0700

    testing

--- code/app/src/main/java/com/example/lotteryeventsystem/OrganizerEventListFragment.java
+++ code/app/src/main/java/com/example/lotteryeventsystem/OrganizerEventListFragment.java
@@ -15,12 +15,14 @@ import androidx.fragment.app.Fragment;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 
+import com.google.firebase.firestore.FieldPath;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.QueryDocumentSnapshot;
 
 import org.jetbrains.annotations.NotNull;
 
 import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
 
 import androidx.annotation.Nullable;
@@ -34,10 +36,6 @@ import androidx.recyclerview.widget.RecyclerView;
 public class OrganizerEventListFragment extends Fragment {
     private HomeEventAdapter eventAdapter;
     private ArrayList<EventItem> eventsList;
-    private EventRepository eventRepository;
-
-    private static ArrayList<Event> cachedItems;
-    private String androidDeviceId;
 
     @Nullable
     @Override
@@ -47,44 +45,53 @@ public class OrganizerEventListFragment extends Fragment {
         return inflater.inflate(R.layout.fragment_organizer_event_list, container, false);
     }
 
-    @Override
-    public void onResume() {
-        super.onResume();
-        reloadEvents();
-    }
-
     /**
      * Logic for when the OrganizerEventList View was created
-     * @param view The View returned by {@link #onCreateView(LayoutInflater, ViewGroup, Bundle)}.
+     *
+     * @param view               The View returned by {@link #onCreateView(LayoutInflater, ViewGroup, Bundle)}.
      * @param savedInstanceState If non-null, this fragment is being re-constructed
-     * from a previous saved state as given here.
+     *                           from a previous saved state as given here.
      */
     public void onViewCreated(@NotNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
 
-        eventRepository = new EventRepository();
-
-        ListView eventListView = view.findViewById(R.id.organizer_event_list);
         Button createEventButton = view.findViewById(R.id.create_event_button);
         ImageButton backButton = view.findViewById(R.id.back_button);
 
-        //eventsList = new ArrayList<Event>();
-        //eventAdapter = new ArrayAdapter<>(requireContext(), android.R.layout.simple_list_item_1, eventsList);
-        //eventListView.setAdapter(eventAdapter);
-
         eventsList = new ArrayList<>();
+
         eventAdapter = new HomeEventAdapter(item -> {
             Bundle args = new Bundle();
-            args.putString(EventDetailFragment.ARG_EVENT_ID, item.id);
+            args.putString("EVENT_ID", item.id);
             NavController navController = Navigation.findNavController(requireView());
-            navController.navigate(R.id.action_organizerEventListFragment_to_eventDetailFragment, args);
+            navController.navigate(R.id.action_organizerEventListFragment_to_organizerEntrantListFragment, args);
         });
         RecyclerView recyclerView = view.findViewById(R.id.events_recycler);
         recyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
         recyclerView.setAdapter(eventAdapter);
 
-        androidDeviceId = Settings.Secure.getString(requireContext().getContentResolver(), Settings.Secure.ANDROID_ID);
-        reloadEvents();
+        FirebaseFirestore db = FirebaseFirestore.getInstance();
+        String deviceId = Settings.Secure.getString(getActivity().getContentResolver(), Settings.Secure.ANDROID_ID);
+        db.collection("users").document(deviceId).get()
+                .addOnSuccessListener(users -> {
+                    List<String> organized_events = (List<String>) users.get("organized_events");
+                    if (organized_events == null) {
+                        return;
+                    }
+                    db.collection("events")
+                            .whereIn(FieldPath.documentId(), organized_events).get()
+                            .addOnSuccessListener(events_user_organized -> {
+                                if (events_user_organized.isEmpty()) {
+                                    return;
+                                }
+                                for (QueryDocumentSnapshot event : events_user_organized) {
+                                    EventItem item = EventItem.queryDocumentSnapshotToEventItem(event);
+                                    eventsList.add(item);
+                                }
+                                eventAdapter.submitList(eventsList);
+                                eventAdapter.notifyDataSetChanged();
+                            });
+                });
 
         // Navigates to the event creation form
         createEventButton.setOnClickListener(v -> {
@@ -92,37 +99,9 @@ public class OrganizerEventListFragment extends Fragment {
             navController.navigate(R.id.action_organizerEventListFragment_to_organizerEventCreateFragment);
         });
 
-        // List View On Click Listener - Passes eventId to the organizerEventListFragment
-        eventListView.setOnItemClickListener((parent, itemView, position, id) -> {
-            EventItem event = eventsList.get(position);
-            Bundle args = new Bundle();
-            args.putString("EVENT_ID", event.id);
-
-            NavController navController = Navigation.findNavController(requireView());
-            navController.navigate(R.id.action_organizerEventListFragment_to_organizerEntrantListFragment, args);
-        });
-
         backButton.setOnClickListener(v -> {
             NavController navController = Navigation.findNavController(view);
             navController.navigateUp(); // Navigates back to the previous fragment
         });
-    }
-
-    private void reloadEvents() {
-        eventsList.clear();
-
-        eventRepository.getEventsByOrganizer(androidDeviceId)
-                .addOnSuccessListener(snapshot -> {
-                    eventsList.clear();
-                    for (var doc : snapshot) {
-                        EventItem item = EventItem.queryDocumentSnapshotToEventItem(doc);
-                        eventsList.add(item);
-                    }
-                    eventAdapter.submitList(eventsList);
-                    eventAdapter.notifyDataSetChanged();
-
-                });
-
-
     }
 }
